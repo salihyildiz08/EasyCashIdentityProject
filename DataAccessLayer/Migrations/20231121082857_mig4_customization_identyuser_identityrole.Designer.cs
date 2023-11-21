0d952fe235e67bda117b59b3e4429ad66e950408
Salih YILDIZ <yldzsalih27@gmail.com>
2023-11-21 11:31:20 +0300

    in AspnetUser- Adding Name,Surname,ImageUrl ...

--- DataAccessLayer/Migrations/20231121082857_mig4_customization_identyuser_identityrole.Designer.cs
+++ DataAccessLayer/Migrations/20231121082857_mig4_customization_identyuser_identityrole.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231121082857_mig4_customization_identyuser_identityrole")]
+    partial class mig4_customization_identyuser_identityrole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
